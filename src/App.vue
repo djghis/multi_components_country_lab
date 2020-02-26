3f8a4dc5f92f4725c7cee556545122735413f9a7
djghis <djghis@hotmail.com>
2020-02-26 21:07:30 +0000

    list of countries displayed on page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,46 @@
 <template>
   <div id="app">
-
+    <h1>Countries</h1>
+    <div class="main-container">
+      <countries-list :countries='countries'></countries-list>
+    </div>
   </div>
 </template>
 
 <script>
+import CountriesList from './components/CountriesList.vue';
+
 
 
 export default {
   name: 'App',
+  data(){
+    return {
+      countries: []
+    };
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+  },
   components: {
-    
+    "countries-list": CountriesList
   }
 }
 </script>
 
 <style>
-#app {
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+  }
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
